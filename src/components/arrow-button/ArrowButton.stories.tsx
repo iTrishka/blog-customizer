067ace3d89197460c81065c0bc9d42fa97e605d9
Kristina Khramova <snowflak@yandex.ru>
2024-05-04 20:10:19 +0300

    feat: added the functionality of the ArticleParamsForm

--- src/components/arrow-button/ArrowButton.stories.tsx
+++ src/components/arrow-button/ArrowButton.stories.tsx
@@ -13,7 +13,10 @@ export const ArrowButtonStory: Story = {
 	render: () => {
 		return (
 			<>
-				<ArrowButton />
+				<ArrowButton
+					toggleParamForm={() => console.log('Story')}
+					isOpen={false}
+				/>
 			</>
 		);
 	},
